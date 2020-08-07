12b82530c6420a209c3c76bbd391d23dfa60126c
root <joelnidoy9@gmail.com>
2020-08-07 09:24:06 -0400

    chaged app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          malupitan
+          malupitan tayo sir !!!!!
         </p>
         <a
           className="App-link"
